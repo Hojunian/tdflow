06d75a6b13f3af31b77d2370fa62783aee5cd4bf
Junseo <greg3566@gmail.com>
2025-10-04 03:25:03 +0900

    encoder

--- model/encoder.py
+++ model/encoder.py
@@ -4,7 +4,9 @@ from einops import rearrange
 from diffusers import FlaxAutoencoderKL
 
 class Encoder(nnx.Module):
-    def __init__(self):
+    def __init__(
+            self,
+        ):
         module, params = FlaxAutoencoderKL.from_pretrained(
             "stabilityai/stable-diffusion-xl-base-1.0",
             subfolder = "vae",
@@ -14,27 +16,27 @@ class Encoder(nnx.Module):
             lambda p: nnx.Param(p), params
         )
     
-    def encode(self, images):
-        params = jax.tree_util.tree_map(
-            lambda p: p.value, self.params
-        )
+    def encode(self, images, key):
+        images = (images - 0.5) / 0.5
         images = rearrange(images, "b h w c -> b c h w")
-        latents = self.module.apply(
-            {"params": params}, images, method=self.module.encode
-        ).latent_dist.sample()
+        latent_dist = self.module.apply(
+            {"params": self.params}, images, method=self.module.encode
+        ).latent_dist
+        latents = latent_dist.sample(key)
         latents *= self.module.config.scaling_factor
-        return latents
-    
+        images = rearrange(images, "b c h w -> b h w c")
+        return latents, latent_dist
+
     def decode(self, latents):
+        latents = rearrange(latents, "b h w c -> b c h w")
         latents /= self.module.config.scaling_factor
-        params = jax.tree_util.tree_map(
-            lambda p: p.value, self.params
-        )
         images = self.module.apply(
-            {"params": params}, latents, method=self.module.decode
+            {"params": self.params}, latents, method=self.module.decode
         ).sample
         images = rearrange(images, "b c h w -> b h w c")
+        images = images * 0.5 + 0.5
         return images
     
-    def __call__(self, x):
-        return self.encode(x)
+    def __call__(self, images, key):
+        latents, _ = self.encode(images, key)
+        return latents
--- train_encoder.py
+++ train_encoder.py
@@ -1,1 +1,78 @@
+import jax
+from jax import numpy as jnp
+from flax import nnx
+import optax
+import flax
+import ogbench
 from model.encoder import Encoder
+from utils.ogbench import Dataset
+
+dataset_name = "visual-scene-play-singletask-v0"
+batch_size = 32
+seed = 0
+log_every = 2
+
+@flax.struct.dataclass
+class TrainingState:
+    model: nnx.Module
+    optimizer: nnx.Optimizer
+
+@nnx.jit
+def train_step(training_state, x_1, key):
+    def loss_fn(model: nnx.Module):
+        z, dist = model.encode(x_1, key)
+        y_1 = model.decode(z)
+        mu = dist.mean
+        logvar = dist.logvar
+
+        loss_mse = jnp.mean((x_1 - y_1) ** 2)
+        loss_kl = 0.5 * jnp.mean(mu ** 2 + jnp.exp(logvar) - logvar - 1)
+        
+        return loss_mse + 1e-6 * loss_kl
+        
+    loss, grads = nnx.value_and_grad(loss_fn)(training_state.model)
+    training_state.optimizer.update(training_state.model, grads)
+    return loss
+
+@nnx.jit
+def val_step(model, x_1, key):
+    z = model(x_1, key)
+    y_1 = model.decode(z)
+    return jnp.mean((x_1 - y_1) ** 2)
+    
+encoder = Encoder()
+optimizer = nnx.Optimizer(
+    encoder,
+    optax.adamw(
+        learning_rate=1e-4
+    ),
+    wrt=nnx.Param,
+)
+training_state = TrainingState(encoder, optimizer)
+train_step_cached = nnx.cached_partial(train_step, training_state)
+val_step_cached = nnx.cached_partial(val_step, training_state.model)
+
+_, train_dataset, val_dataset = ogbench.make_env_and_datasets(dataset_name)
+train_ds = Dataset.create(**train_dataset)
+val_ds = Dataset.create(**val_dataset)
+
+key = jax.random.key(seed)
+
+loss_sum = 0
+for step in range(log_every*100):
+    batch = train_ds.sample(batch_size)
+    x_1 = batch["observations"].astype(jnp.float32) / 255.0
+
+    key, key_update = jax.random.split(key)
+    loss = train_step_cached(x_1, key)
+
+    loss_sum += loss
+    if step%log_every == (log_every-1):
+        train_loss = loss_sum/log_every
+
+        val_batch = val_ds.sample(batch_size)
+        val_x = val_batch["observations"].astype(jnp.float32) / 255.0
+        val_loss = val_step_cached(val_x, key)
+
+        print(step, train_loss, val_loss)
+        loss_sum = 0
